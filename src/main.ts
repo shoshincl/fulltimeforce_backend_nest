95683fef2394ab84d240a4bb2d87a995395268d7
Diego Robles <shoshincl@gmail.com>
2023-07-26 00:16:14 -0400

    wip(main):added dotenv, app module with dummy controller

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppModule } from './app.module';
+
+ConfigModule.forRoot();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.APP_PORT);
 }
 bootstrap();
